b368f49ef16d4ece7e9cb425f80cf57056705f19
AlexandrEremin <Eremin_-_Sasha@mail.ru>
2022-12-24 17:02:10 +0500

    parallel processing of requests in a queue

--- src/main/java/com/example/aliarslan/basickafkademo/service/KafkaConsumerService.java
+++ src/main/java/com/example/aliarslan/basickafkademo/service/KafkaConsumerService.java
@@ -3,6 +3,7 @@ package com.example.aliarslan.basickafkademo.service;
 import com.example.aliarslan.basickafkademo.model.KafkaMessage;
 import com.example.aliarslan.basickafkademo.tasks.Iwork;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -18,9 +19,11 @@ import java.util.concurrent.Future;
 @Slf4j
 public class KafkaConsumerService {
 
-    public ExecutorService threadPool = Executors.newFixedThreadPool(2);
 
-    public List<Future<KafkaMessage>> futures = new ArrayList<>();
+    private static int sizeThreadPool = 4;
+    private static ExecutorService threadPool = Executors.newFixedThreadPool(sizeThreadPool);
+    private static List<Future<KafkaMessage>> futures = new ArrayList<>();
+
     @KafkaListener(topics = "${kafka.topic.name}", groupId = "${kafka.group.id}")
     public void listen(@Payload KafkaMessage kafkaMessage) {
         futures.add(
--- src/main/java/com/example/aliarslan/basickafkademo/tasks/SendMessageTask.java
+++ src/main/java/com/example/aliarslan/basickafkademo/tasks/SendMessageTask.java
@@ -1,20 +1,12 @@
 package com.example.aliarslan.basickafkademo.tasks;
 
 import com.example.aliarslan.basickafkademo.model.KafkaMessage;
-import com.example.aliarslan.basickafkademo.service.KafkaConsumerService;
 import com.example.aliarslan.basickafkademo.service.KafkaProducerService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
 
-import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.function.Supplier;
 
 @Component
